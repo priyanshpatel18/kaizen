411484522c622a174c5f5a9bdc512d5eed4413f5
priyanshpatel18 <priyansh.180804@gmail.com>
2024-12-22 23:27:42 +0530

    changed the encryption to cryptography based

--- src/lib/encrypt.ts
+++ src/lib/encrypt.ts
@@ -1,10 +1,10 @@
-import { EncryptJWT, SignJWT, jwtDecrypt, jwtVerify } from "jose";
+import { CompactEncrypt, SignJWT, compactDecrypt, importJWK, jwtVerify } from "jose";
 
 // Load keys from environment variables
-const KEY = process.env.NEXT_PUBLIC_SECRET || "";
+const publicKeyBase64 = process.env.NEXT_PULIC_PUBLIC_KEY || "";
+const privateKeyBase64 = process.env.PRIVATE_KEY || "";
 
-const BASE_URL = process.env.NEXTAUTH_URL || "http://localhost:3000";
-const SECRET_KEY = new TextEncoder().encode(KEY);
+const SECRET_KEY = new TextEncoder().encode(process.env.NEXTAUTH_SECRET || "");
 
 // Generate a JWT token
 export const generateToken = async (payload: Record<string, any>, expiresIn: string = "1h") => {
@@ -17,25 +17,36 @@ export const verifyToken = async (token: string) => {
   return payload as Record<string, any>;
 };
 
+async function loadKeys() {
+  const publicKeyJwk = JSON.parse(Buffer.from(publicKeyBase64, "base64").toString("utf-8"));
+
+  const privateKeyJwk = JSON.parse(Buffer.from(privateKeyBase64, "base64").toString("utf-8"));
+
+  const publicKey = await importJWK(publicKeyJwk, "RSA-OAEP");
+  const privateKey = await importJWK(privateKeyJwk, "RSA-OAEP");
+
+  return { publicKey, privateKey };
+}
+
 // Encrypt data using the public key
 export const encryptData = async (data: Record<string, any>) => {
-  const ENCODING_KEY = Uint8Array.from(atob(KEY), (c) => c.charCodeAt(0));
+  const encoder = new TextEncoder();
+  const { publicKey } = await loadKeys();
 
-  return await new EncryptJWT(data)
-    .setProtectedHeader({ alg: "dir", enc: "A256GCM" })
-    .setIssuedAt()
-    .setAudience(BASE_URL)
-    .setIssuer(BASE_URL)
-    .setExpirationTime("2h")
-    .encrypt(ENCODING_KEY);
+  const encrypted = await new CompactEncrypt(encoder.encode(JSON.stringify(data)))
+    .setProtectedHeader({ alg: "RSA-OAEP", enc: "A256GCM" })
+    .encrypt(publicKey);
+
+  return encrypted;
 };
 
 // Decrypt data using the private key
 export const decryptData = async (encryptedData: string) => {
-  const ENCODING_KEY = Uint8Array.from(atob(KEY), (c) => c.charCodeAt(0));
+  const { privateKey } = await loadKeys();
 
-  return await jwtDecrypt(encryptedData, ENCODING_KEY, {
-    audience: BASE_URL,
-    issuer: BASE_URL,
-  });
+  const { plaintext } = await compactDecrypt(encryptedData, privateKey);
+  const decoder = new TextDecoder();
+  const decodedData = JSON.parse(decoder.decode(plaintext));
+
+  return decodedData;
 };
